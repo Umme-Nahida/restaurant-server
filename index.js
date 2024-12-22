4295698d3f14c56c647921795ab936a55982ddbe
Developer Nahida <ummenahida20@gmail.com>
2024-12-22 19:37:15 +0600

    booking api added hare

--- index.js
+++ index.js
@@ -278,6 +278,9 @@ async function run() {
     }catch(err){
       console.log(err)
     }
+
+
+    //booking api added here 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
