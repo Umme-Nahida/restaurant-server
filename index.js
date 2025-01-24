462e4099ac4e6b4b3d136c6004c30d53e305eefb
Developer Nahida <ummenahida20@gmail.com>
2025-01-25 00:34:10 +0600

    live link added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json())
 app.use(cors({
-  origin: ["http://localhost:5173"]
+  origin: ["http://localhost:5173","https://peppy-valkyrie-6c037b.netlify.app"]
 }))
 
 const varifyToken = (req, res, next) => {
